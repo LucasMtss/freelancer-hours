de9671f29b10118dc0c2b1cbdd31b3a191a2c128
LucasMtss <lucasrmts@gmail.com>
2025-01-18 13:13:49 -0300

    feat: add proposals model

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Project;
+use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -19,7 +20,10 @@ class DatabaseSeeder extends Seeder
 
         // Pesquisa 10 usuários no banco para inserir no campo created_by do projeto que irá ser criado
         User::query()->inRandomOrder()->limit(10)->get()
-            ->each(fn(User $u) => Project::factory()->create(['created_by' => $u->id])
-        );
+            ->each(function (User $u) { 
+                $project = Project::factory()->create(['created_by' => $u->id]);
+
+                Proposal::factory()->count(random_int(4,45))->create(['project_id' => $project->id]);
+        });
     }
 }
