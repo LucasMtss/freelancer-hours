b836e68e51ee91f282bfe532166da97b5d404f16
LucasMtss <lucasrmts@gmail.com>
2025-01-18 15:47:37 -0300

    feat: add livewire components

---
+++ resources/views/livewire/projects/index.blade.php
@@ -0,0 +1,10 @@
+<div>
+    @foreach($projects as $project)
+    <li>
+        <a href="{{ route('projects.show', $project) }}">
+
+            {{ $project->id }}. {{ $project->title }}
+        </a>
+    </li>
+    @endforeach
+</div>
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.app>
+    show
+    <livewire:projects.show :project="$project"/>
+    <livewire:projects.proposals :project="$project"/>
+</x-layouts.app>
