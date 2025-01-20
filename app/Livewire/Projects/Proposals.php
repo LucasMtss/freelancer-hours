add49c70981c9e0e6566d1a315d97bca65f7decc
LucasMtss <lucasrmts@gmail.com>
2025-01-20 07:18:48 -0300

    feat: add changes of 2 classroom

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Livewire\Projects;
+use Livewire\Attributes\Computed;
 
 use App\Models\Project;
 use Livewire\Component;
@@ -11,5 +12,12 @@ class Proposals extends Component
     public function render()
     {
         return view('livewire.projects.proposals');
+    }
+    #[Computed()]
+    public function lastProposalTime()
+    {
+        return $this->project->proposals()
+            ->latest()->first()
+            ->created_at->diffForHumans();
     }
 }
